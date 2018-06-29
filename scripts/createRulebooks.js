7a03db3ebd64614608551bcf5b045208034a94ba
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-29 13:06:17 +0100

    feat(handbooker): upgraded handbooker and usage of the package

--- scripts/createRulebooks.js
+++ scripts/createRulebooks.js
@@ -4,7 +4,6 @@ const { handbooker, } = require( "handbooker" );
 
 const options = {
 	debug: true,
-	style: "./node_modules/handbooker/lib/styles/homebrewery-styles.css",
 	printOptions: {
 		displayHeaderFooter: false,
 	},
